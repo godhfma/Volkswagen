cc43202e36faa47b8801c1f15265fd888f17e8e7
godhfma <bluemjh25@gmail.com>
2021-07-15 17:37:11 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
-jQuery(function($){
-    $(".toggle-btn").click(function(){
-        $(".gnb").stop().slideToggle("fast"); 
+    $(document).ready(function(){
+        // menu 클래스 바로 하위에 있는 a 태그를 클릭했을때
+        $('.fmenu__li').click(function(){
+            let submenu = $(this).next('ul');
+ 
+            // submenu 가 화면상에 보일때는 위로 보드랍게 접고 아니면 아래로 보드랍게 펼치기
+            if( submenu.is(":visible") ){
+                submenu.slideUp(200);
+            }else{
+                submenu.slideDown(200);
+            }
+        });
+        
+            $('.toggle-btn').click(function(){
+            $('.gnb').stop().slideToggle('fast'); 
     });
-});
+    });
